f6829d1446499241e4e3c06254714b53cf12f1c4
oyghen <oyghen@duck.com>
2025-12-30 10:27:05 +0100

    refactor(cli): capitalize version option

--- src/purechance/cli.py
+++ src/purechance/cli.py
@@ -16,7 +16,7 @@ SEED_OPT = typer.Option(None, help="Seed for the random number generator.")
 @app.callback(invoke_without_command=True)
 def version(
     show: bool = typer.Option(
-        False, "--version", "-v", help="Show app version and exit."
+        False, "--version", "-V", help="Show app version and exit."
     ),
 ) -> None:
     if show:
